e9b33ce72229d20ec6134913fbc5a96c793d73ce
RDCoder8 <mojinr@gmail.com>
2023-07-24 22:35:15 -0400

    Coding classes.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //FullStackStory
+/* 
+I want to make a turn-based, story battle that displays text in a box, has two actors on field, and changes the ending of the story based on whether the player has defeated the opposing actor or not through the choices they made. 
+
+First I will need classes for the player (New Software Engineer) and CodeBeast (Opposing Actor). They will need properties such as HP and Attack. Also because it is a story, there will be dialogue or lines said between each attack cycle/round. 
+
+I will need a way to store the text lines and show them on the screen. Also I will likely need a way to clear them based on the player pushing a key or clicking.
+
+Win-lose conditions will determine the ending of the story but the player can select different methods from a menu to attack the CodeBeast.
+
+Player Attack Methods include Attack, Research, Summon, and Procrasinate.
+
+*/
